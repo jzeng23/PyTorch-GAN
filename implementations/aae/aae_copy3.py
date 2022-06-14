17262d565422838f2b3e55e2ab3a674888f3e9b3
Julia Zeng <julia.z@wustl.edu>
2022-06-14 13:29:12 -0500

    add top layer

--- implementations/aae/aae_copy3.py
+++ implementations/aae/aae_copy3.py
@@ -7,13 +7,16 @@ import itertools
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision import datasets
 from torch.autograd import Variable
+from torch.utils.tensorboard import SummaryWriter
 
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+
+from topologylayer.nn import LevelSetLayer2D, SumBarcodeLengths
 
 os.makedirs("images", exist_ok=True)
 
@@ -27,7 +30,8 @@ parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads 
 parser.add_argument("--latent_dim", type=int, default=10, help="dimensionality of the latent code")
 parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+parser.add_argument("--sample_interval", type=int, default=13, help="interval between image sampling")
+parser.add_argument("--save_dir", type=str, default='best_model3', help="directory where you save models")
 opt = parser.parse_args()
 print(opt)
 
@@ -121,20 +125,20 @@ if cuda:
     adversarial_loss.cuda()
     pixelwise_loss.cuda()
 
-# Configure data loader
+# Configure data loaders
 os.makedirs("../../data/mnist", exist_ok=True)
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST(
+dataset = datasets.MNIST(
         "../../data/mnist",
         train=True,
         download=True,
         transform=transforms.Compose(
             [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
         ),
-    ),
-    batch_size=opt.batch_size,
-    shuffle=True,
-)
+    )
+dataset, _ = random_split(dataset, [1000, 59000])
+train, valid = random_split(dataset, [800, 200])
+trainloader = torch.utils.data.DataLoader(train, batch_size=opt.batch_size, shuffle=True)
+validloader = torch.utils.data.DataLoader(valid, batch_size=opt.batch_size, shuffle=True)
 
 # Optimizers
 optimizer_G = torch.optim.Adam(
@@ -144,21 +148,35 @@ optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
+levelsetlayer = LevelSetLayer2D(size=(opt.img_size, opt.img_size), maxdim=1, sublevel=False)
+beta0layer = SumBarcodeLengths(dim=0)
 
-def sample_image(n_row, batches_done):
+def sample_image(n_row, batches_done, current_epoch):
     """Saves a grid of generated digits"""
     # Sample noise
     z = Variable(Tensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
     gen_imgs = decoder(z)
-    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
+    save_image(gen_imgs.data, "imagest3/epoch_%d.png" % current_epoch, nrow=n_row, normalize=True)
 
+
+# initialize SummaryWriter
+writer = SummaryWriter()
+
+# variables for tracking epoch with best validation performance
+best_epoch = 0
+best_val_loss = np.inf
 
 # ----------
 #  Training
 # ----------
-
 for epoch in range(opt.n_epochs):
-    for i, (imgs, _) in enumerate(dataloader):
+    epoch_g_loss = 0
+    epoch_top_loss = 0
+    epoch_d_loss = 0
+    encoder.train()
+    decoder.train()
+    discriminator.train()
+    for i, (imgs, _) in enumerate(trainloader):
 
         # Adversarial ground truths
         valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
@@ -175,12 +193,20 @@ for epoch in range(opt.n_epochs):
 
         encoded_imgs = encoder(real_imgs)
         decoded_imgs = decoder(encoded_imgs)
+        # decoded_imgs = tensor of shape [64, 1, 32, 32]
+
+        top_loss = 0
+        for n in range(decoded_imgs.size(0)):
+            barcode = levelsetlayer(decoded_imgs[n, 0, :, :])
+            top_loss += beta0layer(barcode)
+        top_loss = top_loss / (100*decoded_imgs.size(0))
+        epoch_top_loss += top_loss
 
         # Loss measures generator's ability to fool the discriminator
-        g_loss = 0.001 * adversarial_loss(discriminator(encoded_imgs), valid) + 0.999 * pixelwise_loss(
+        g_loss = top_loss + 0.001 * adversarial_loss(discriminator(encoded_imgs), valid) + 0.999 * pixelwise_loss(
             decoded_imgs, real_imgs
         )
-
+        epoch_g_loss += g_loss.item()
         g_loss.backward()
         optimizer_G.step()
 
@@ -198,14 +224,67 @@ for epoch in range(opt.n_epochs):
         fake_loss = adversarial_loss(discriminator(encoded_imgs.detach()), fake)
         d_loss = 0.5 * (real_loss + fake_loss)
 
+        epoch_d_loss += d_loss.item()
         d_loss.backward()
         optimizer_D.step()
-
         print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+            "[Epoch %d/%d] Training [Batch %d/%d] [D loss: %f] [G loss: %f]"
+            % (epoch, opt.n_epochs, i, len(trainloader), d_loss.item(), g_loss.item())
         )
 
-        batches_done = epoch * len(dataloader) + i
+        batches_done = epoch * len(trainloader) + i
         if batches_done % opt.sample_interval == 0:
-            sample_image(n_row=10, batches_done=batches_done)
+            sample_image(n_row=10, batches_done=batches_done, current_epoch=epoch)
+
+    # -----------
+    # Validation
+    # -----------
+    encoder.eval()
+    decoder.eval()
+    discriminator.eval()
+    epoch_valid_g_loss = 0
+    epoch_valid_top_loss = 0
+    epoch_valid_d_loss = 0
+    for j, (valid_imgs, _) in enumerate(validloader):
+        valid = Variable(Tensor(valid_imgs.shape[0], 1).fill_(1.0), requires_grad=False)
+        real_imgs = Variable(valid_imgs.type(Tensor))
+        encoded_imgs = encoder(real_imgs)
+        decoded_imgs = decoder(encoded_imgs)
+        top_loss = 0
+        for m in range(decoded_imgs.size(0)):
+            barcode = levelsetlayer(decoded_imgs[m, 0, :, :])
+            top_loss += beta0layer(barcode)
+        top_loss = top_loss / (100*decoded_imgs.size(0))
+        epoch_valid_top_loss += top_loss
+        valid_g_loss = top_loss + 0.001 * adversarial_loss(discriminator(encoded_imgs), valid) + 0.999 * pixelwise_loss(
+            decoded_imgs, real_imgs
+        )
+        epoch_valid_g_loss += valid_g_loss.item()
+
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+
+        print(
+            "[Epoch %d/%d] Validation [Batch %d/%d] [G loss: %f]"
+            % (epoch, opt.n_epochs, j, len(validloader), valid_g_loss.item())
+        )
+
+    epoch_valid_g_loss /= len(validloader)
+    if epoch_valid_g_loss < best_val_loss:
+        best_epoch = epoch
+        best_val_loss = epoch_valid_g_loss
+        torch.save(encoder, os.path.join(opt.save_dir, 'encoder_%03d.pth' % (epoch)))
+        torch.save(decoder, os.path.join(opt.save_dir, 'decoder_%03d.pth' % (epoch)))
+        torch.save(discriminator, os.path.join(opt.save_dir, 'discriminator_%03d.pth' % (epoch)))
+        
+    writer.add_scalars('Training Loss100', {
+            'Generator': epoch_g_loss / len(trainloader),
+            'Discriminator': epoch_d_loss / len(trainloader),
+            'Topology': epoch_top_loss / len(trainloader)
+        }, epoch)
+
+    writer.add_scalars('Valid Loss100', {
+            'Generator': epoch_valid_g_loss,
+            'Topology': epoch_valid_top_loss / len(validloader)
+        }, epoch)
+
+    writer.add_text('Best Epoch100', str(best_epoch))
